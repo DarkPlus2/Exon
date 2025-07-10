0af7a74134954f1d7a77951efe1c400404e6aa33
DarkPlusX <158550285+DarkPlus2@users.noreply.github.com>
2025-07-10 15:36:46 +0600

    Create config.js

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  inviteURL: "https://discord.com/oauth2/authorize?client_id=YOUR_ID&scope=bot",
+  supportURL: "https://discord.gg/support",
+  docsURL: "https://yourbot.docs.io",
+  stats: {
+    servers: "2,450",
+    users: "87,320",
+    songs: "1.2M+"
+  }
+};
